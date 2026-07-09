d16d3606fbd49e004456dbd4787dfabe371ebfdd
Tracee <traceemiasco2501@gmail.com>
2026-07-09 10:49:20 -0700

    fix: change application name on header to instantwrite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 export default function Header() {
     return (
         <div id="header">
-            <h2>Welcome to Instawrite</h2>
+            <h2>Welcome to Instantwrite</h2>
         </div>
     )
 }
