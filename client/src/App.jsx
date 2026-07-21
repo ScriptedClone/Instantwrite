71108ec0e4dc21c374b9f713931bdd88b95c305b
Tracee <traceemiasco2501@gmail.com>
2026-07-20 20:09:26 -0700

    add: include login page to router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import { Routes, Route } from "react-router";
 import EditorPage from "./pages/EditorPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 import './App.css'
 
 export default function App() {
     return (
         <>
             <Routes>
-                <Route path="/signup" element={<SignupPage />}/>
                 <Route path="/" element={<HomePage />}/>
+                <Route path="/signup" element={<SignupPage />}/>
+                <Route path="/login" element={<LoginPage />}/>
                 <Route path="/editor" element={<EditorPage />}/>
             </Routes>
         </>
