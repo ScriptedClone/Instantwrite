478ea760a1c506dba8c1882f69bcdd67055730f3
Tracee <traceemiasco2501@gmail.com>
2026-08-05 17:10:58 -0700

    feature: enhance filetree buttons UX/UI

--- client/src/features/file-tree/FileTreeHeader.jsx
+++ client/src/features/file-tree/FileTreeHeader.jsx
@@ -1,9 +1,24 @@
+import textIcon from "./assets/textIcon.png"
+import folderIcon from "./assets/folderIcon.png"
+import saveIcon from "./assets/saveIcon.png"
+
 export default function FileTreeHeader({handleHeaderBtn}) {
     return(
         <div className="fileTreeHeader">
-            <button onClick={() => handleHeaderBtn("document")}>TEXT</button>
-            <button onClick={() => handleHeaderBtn("folder")}>FOLDER</button>
-            <button onClick={() => handleHeaderBtn("save")}>SAVE</button>
+            <button className="fileTreeHeaderBtn" onClick={() => handleHeaderBtn("document")}>
+                <img className="fileTreeHeaderIcon" src={textIcon}/>
+                <span>Text</span>
+            </button>
+
+            <button className="fileTreeHeaderBtn" onClick={() => handleHeaderBtn("folder")}>
+                <img className="fileTreeHeaderIcon" src={folderIcon}/>
+                <span>Folder</span>
+            </button>
+
+            <button className="fileTreeHeaderBtn" onClick={() => handleHeaderBtn("save")}>
+                <img className="fileTreeHeaderIcon" src={saveIcon}/>
+                <span>Save</span>
+            </button>
         </div>
     )
 }
