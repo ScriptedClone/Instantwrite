54dc13a820b1dc230fa89a529b1a427779ed356b
Tracee <traceemiasco2501@gmail.com>
2026-08-19 16:05:09 -0700

    refactor: merge Editor component to EditorPanel - To maintain feature panel components as owner of states and actions

--- client/src/features/editor/EditorPanel.jsx
+++ client/src/features/editor/EditorPanel.jsx
@@ -1,18 +1,59 @@
+import { extensions } from "./config/extensions.js"
+import { EditorContent, useEditor } from "@tiptap/react"
+import { useEffect } from "react";
 import EditorTitle from "./EditorTitle.jsx";
-import Editor from "./Editor.jsx";
 import './EditorPanel.css'
 
 export default function EditorPanel({handleEditorTxtUpdate, 
                                      handleSelection, 
                                      selectedDoc,
                                      docName}) {
+    const editor = useEditor({
+        extensions: extensions,
+        content: "",
+        autofocus: true,
+    })
+
+    /** Set editor to selected document in file tree */
+    useEffect(() => {
+        if(!selectedDoc || !editor) return;
+        editor.commands.setContent(selectedDoc)
+    }, [selectedDoc])
+    
+    /** 
+     * Gets editor and converts to JSON for every
+     * editor text update.
+     */
+    useEffect(() => {
+        function handleEditorUpdate() {
+            handleEditorTxtUpdate(editor.getJSON());
+        }
+
+        editor.on('update', handleEditorUpdate);
+
+        return () => editor.off('update', handleEditorUpdate)
+    }, [])
+
+    /** 
+     * Pass generated selection object to handler
+     */
+    useEffect(() => {
+        function handleSelectionUpdate() {
+            if (editor.state.selection.empty) return
+            const selection = editor.state.selection
+
+            handleSelection(selection);
+        }
+
+        editor.on('selectionUpdate', handleSelectionUpdate);
+
+        return () => editor.off('selectionUpdate', handleSelectionUpdate);
+    }, [])
 
     return (
         <div className="editorPanel">
             <EditorTitle docName={docName}/>
-            <Editor selectedDoc={selectedDoc}
-                    handleEditorTxtUpdate={handleEditorTxtUpdate}
-                    handleSelection={handleSelection}/>
+            <EditorContent className ="editor" editor={editor}/>
         </div>
     )
 
