5ee93030d90a8daa0d677f9f2fb45ef0d974bd19
Tracee Miasco <traceemiasco2501@gmail.com>
2026-09-12 09:16:45 -0700

    fix: prevent file tree crash
    - crash while building folderChildMap when inserting
    node id to parent folder that doesn't have an entry yet.
    - fixed by building folderChildMap first.

--- server/src/helpers/projectHelpers.js
+++ server/src/helpers/projectHelpers.js
@@ -10,20 +10,32 @@ export function convertRowsToFileMap(rows) {
     const nodeMap = {}
 
     rows.forEach((row) => {
-        const nodeId = row.node_id
-
         if(row.type === 'folder') {
-            folderChildMap[nodeId] = [];
-            nodeMap[nodeId] = {id: nodeId, type: row.type, name: row.name}
+            folderChildMap[row.node_id] = [];
 
-            if(row.parent_id) {
-                folderChildMap[row.parent_id][row.index] = nodeId;
+            nodeMap[row.node_id] = {
+                id: row.node_id, 
+                type: row.type, 
+                name: row.name
             }
         }
-        if(row.type === 'document') {
-           folderChildMap[row.parent_id][row.index] = nodeId;
-           nodeMap[nodeId] = {id: nodeId, type: row.type, name: row.name, tiptapContent: row.content}
+    })
+
+    rows.forEach((row) => {
+        // Skip root folder which has null parent_id
+        if(row.parent_id) {
+            folderChildMap[row.parent_id][row.index] = row.node_id;
         }
+        
+        if(row.type === 'document') {
+            nodeMap[row.node_id] = {
+                id: row.node_id, 
+                type: row.type, 
+                name: row.name, 
+                tiptapContent: row.content
+            }
+        }
+
     })
 
     return { folderChildMap, nodeMap }
